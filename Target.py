365193bde1424e66a287047374aa519e8e019f30
Holden <qqqwsq@gmail.com>
2021-09-21 21:25:41 +0300

    В конструктор Target добавлена координата Z Свойства X, Y, Z инкапсулированы Для X,Y, Z добавленны функции Get/Set

--- Target.py
+++ Target.py
@@ -3,18 +3,46 @@ import plotly.graph_objects as go
 
 
 class Target:
-    def __init__(self, x_coord, y_coord, trajectory):
-        self.x_coord = x_coord
-        self.y_coord = y_coord
+    def __init__(self, x_coord, y_coord, z_coord,trajectory):
+        self._x_coord = x_coord
+        self._y_coord = y_coord
+        self._z_coord = z_coord
         self._trajectory = trajectory
+
 
     @property
     def trajectory(self):
         return self._trajectory
 
+    @property
+    def x_coord(self):
+        return self.x_coord
+
+    @property
+    def y_coord(self):
+        return self.y_coord
+
+    @property
+    def z_coord(self):
+        return self.z_coord
+
+
+    @x_coord.setter
+    def x_coord(self, value):
+        self.x_coord = value
+
+    @y_coord.setter
+    def y_coord(self, value):
+        self.y_coord = value
+
+    @z_coord.setter
+    def z_coord(self, value):
+        self.z_coord = value
+
     @trajectory.setter
     def trajectory(self, value):
-        self._trajectory = self.trajectoryEval(value[0], value[1], value[2], value[3], value[4])
+        self._trajectory = self.trajectoryEvalds(value[0], value[1], value[2], value[3], value[4])
+
 
     @staticmethod
     def trajectoryEval(altitude, x_direct, y_direct, x_bias, y_bias):
@@ -34,3 +62,7 @@ class Target:
 
         return go.Scatter3d(x=x_curve, y=y_curve, z=z_curve, marker=dict(size=1, color='darkblue'),
                             line=dict(color='darkblue', width=2))
+
+    @y_coord.setter
+    def y_coord(self, value):
+        self._y_coord = value
